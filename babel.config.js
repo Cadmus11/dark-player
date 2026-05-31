e28dd5e5842cd114507c2fa612ba9546fcc60825
cadmus11 <obsidian.north.003@gmail.com>
2026-05-31 11:01:29 +0300

    babel fix

--- babel.config.js
+++ babel.config.js
@@ -1,36 +1,8 @@
-// module.exports = function (api) {
-//   api.cache(true);
-//   let plugins = [];
-
-//   plugins.push('react-native-worklets/plugin');
-
-//   return {
-//     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-
-//     plugins,
-//   };
-// };
-
-// module.exports = function (api) {
-//   api.cache(true);
-
-//   return {
-//     presets: ['babel-preset-expo'],
-//     plugins: ['nativewind/babel', 'react-native-reanimated/plugin', 'react-native-worklets/plugin'],
-//   };
-// };
-
-
-
 module.exports = function (api) {
   api.cache(true);
 
   return {
     presets: ['babel-preset-expo'],
-
-    plugins: [
-      'nativewind/babel',
-      'react-native-reanimated/plugin',
-    ],
+    plugins: ['nativewind/babel', 'react-native-reanimated/plugin'],
   };
 };
